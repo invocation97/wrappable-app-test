11bfbbe42f11c3fd3d1cb19b2fd206e18409efcc
invocation97 <danilo97benovic@gmail.com>
2024-06-17 13:20:35 +0200

    feat: Improve NewCup component with lazy loading for design image and spinner

--- src/components/NewCup.jsx
+++ src/components/NewCup.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Decal, useGLTF, useTexture } from "@react-three/drei";
-import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter";
-import { button, useControls } from "leva";
 import { calculateCenter } from "../utils/calculateCenter";
+import Spinner from "./Spinner";
 
 export function NewCup(props) {
   const [designImage, setDesignImage] = useState("/textures/wrapper.png");
-  const { nodes, materials } = useGLTF("/models/cup.glb");
+  const [isLoading, setIsLoading] = useState(true);
+  const { nodes, materials } = useGLTF("/models/cup.glb", true, undefined, () =>
+    setIsLoading(false)
+  );
   const modelRef = useRef(null);
 
   useEffect(() => {
@@ -17,57 +19,6 @@ export function NewCup(props) {
     }
   }, []);
 
-  function download3DModel() {
-    if (!modelRef.current) return;
-
-    const exporter = new GLTFExporter();
-    exporter.parse(
-      modelRef.current,
-      function (result) {
-        save(result, "scene.glb");
-      },
-      function (error) {
-        console.error("An error happened:", error);
-      },
-      { binary: true }
-    );
-  }
-
-  function save(blob, filename) {
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(
-      new Blob([blob], { type: "application/octet-stream" })
-    );
-    link.download = filename;
-    link.click();
-  }
-
-  //   useControls({
-  //     design: {
-  //       image: { image: "/textures/wrapper.png" },
-  //       label: "Design image",
-  //       onChange: (blob) => {
-  //         setDesignImage(blob);
-  //       },
-  //     },
-  //     // angle: {
-  //     //   min: degToRad(30),
-  //     //   max: degToRad(270),
-  //     //   value: 0,
-  //     //   step: 0.01,
-  //     //   onChange: (value) => {
-  //     //     const x = Math.cos(value);
-  //     //     const z = Math.sin(value);
-  //     //     const rot = Math.atan2(x, z);
-  //     //     setRotation(() => [0, rot, 0]);
-  //     //     setPos((pos) => [x, pos[1], z]);
-  //     //   },
-  //     // },
-  //     export: button(() => {
-  //       console.log("clicked");
-  //       download3DModel();
-  //     }),
-  //   });
   const originalZPositions = {
     topPart: 22.023,
     middlePart: 21.453,
@@ -103,13 +54,14 @@ export function NewCup(props) {
     middlePart: originalXPositions.middlePart - centerX,
   };
 
-  // Calculate the new z positions
   const newZPositions = {
     topPart: originalZPositions.topPart - centerZ,
     middlePart: originalZPositions.middlePart - centerZ,
   };
+
   return (
     <group {...props} dispose={null} ref={modelRef}>
+      {isLoading && <Spinner />}
       <mesh
         castShadow
         receiveShadow
@@ -127,7 +79,9 @@ export function NewCup(props) {
         <Decal
           position={[1.1, 1.1, 3]}
           scale={14.7}
-          map={useTexture(designImage)}
+          map={useTexture(designImage, undefined, undefined, () =>
+            setIsLoading(false)
+          )}
         ></Decal>
       </mesh>
     </group>
