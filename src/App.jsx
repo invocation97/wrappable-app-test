d9b244f2ef3f1ced92eb4f8639b2e4aa1ee7d807
invocation97 <danilo97benovic@gmail.com>
2024-06-18 10:25:40 +0200

    feat: Improve iframe handling in App component
    This commit improves the handling of iframes in the App component. It checks if the page is embedded in an iframe and redirects to "https://wrapware.com" if it is not embedded from "wrapware.com". If the page is not in an iframe, it redirects to "https://wrapware.com". This enhancement ensures that the application is always accessed from the correct source and improves the security and user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { Leva } from "leva";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if (window.self !== window.top) {
+      // The page is in an iframe
+      if (!document.referrer.includes("wrapware.com")) {
+        // The page is not embedded from wrapware.com
+        window.top.location.href = "https://wrapware.com";
+      }
+    } else {
+      // The page is not in an iframe
+      window.location.href = "https://wrapware.com";
+    }
+  }, []);
   return (
     <div style={{ position: "relative", minHeight: "100vh", height: "100%" }}>
       <div style={{ position: "absolute", left: 0, top: 0, zIndex: 99999 }}>
